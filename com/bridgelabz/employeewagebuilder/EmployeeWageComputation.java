b62f1ad77983bafdf01f999259c98a18014cdf34
ravindrahawaldar1997 <hawaldarravindra@gmail.com>
2022-11-24 10:23:14 +0530

    Added: Program for calculating employee till the condition reached

--- com/bridgelabz/employeewagebuilder/EmployeeWageComputation.java
+++ com/bridgelabz/employeewagebuilder/EmployeeWageComputation.java
@@ -76,12 +76,37 @@ public class EmployeeWageComputation {
 	}
 	System.out.println("Total Employee Wage " +TOTAL_EMPLOYEE_WAGE);
 	}
+	void wagesTillConditionsReached() {
+		int TOTAL_EMPLOYEE_HOUR=0;
+		int TOTAL_WORKING_DAYS=0;
+		int MAXIMUM_HOURS_IN_MONTH=100;
+		while(TOTAL_EMPLOYEE_HOUR<= MAXIMUM_HOURS_IN_MONTH &&
+				TOTAL_WORKING_DAYS<NUMBER_OF_WORKING_DAYS) {
+			TOTAL_WORKING_DAYS++;
+			int employeeCheck=(int)Math.floor(Math.random() *10) %3;
+			 switch(employeeCheck) {
+			 case FULL_TIME :
+				 EMPLOYEE_HOURS=16;
+				 break;
+			 case PART_TIME :
+				 EMPLOYEE_HOURS=8;
+				 break;
+				 default:
+					 EMPLOYEE_HOURS=0;
+				  }
+			 TOTAL_EMPLOYEE_HOUR += EMPLOYEE_HOURS;
+			 System.out.println("Day#: " + TOTAL_WORKING_DAYS + " EmpHour: " +EMPLOYEE_HOURS);
+		}
+		int totalEmployeeWage= TOTAL_EMPLOYEE_HOUR * EMPLOYEE_RATE_PER_HOUR;
+		System.out.println("Total Employee Wage " +totalEmployeeWage);
+			
+		}
 	public static void main(String[] args) {
 		EmployeeWageComputation employeewage = new EmployeeWageComputation();
 		//employeewage.checkEmployeeAttendence();
 		//employeewage.employeeDailyWage();
 		//employeewage.partTimeEmployeeWage();
 		//employeewage.employeeWageUsingSwitchCase();		
-		employeewage.monthlyEmployeeWage();
-	}
+		//employeewage.monthlyEmployeeWage();
+		employeewage.wagesTillConditionsReached();	}
 }
